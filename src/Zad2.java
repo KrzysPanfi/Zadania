e165a368beaf22ef819db38e046be487799ce50c
Krzysztof <krzyspanfilewicz@gmail.com>
2024-02-22 11:28:59 +0100

    Dodano rand

--- src/Zad2.java
+++ src/Zad2.java
@@ -3,7 +3,8 @@ import java.util.Random;
 public class Zad2 {
     public  void Random_array(){
         Random rand =new Random();
-
+int size= rand.nextInt(10-20);
+int[][] random_array=new int[size][size];
     }
 
 }
