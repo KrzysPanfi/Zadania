220d092f90eade22cd2c95ab38708d0716a4e3bb
Krzysztof <krzyspanfilewicz@gmail.com>
2024-02-22 16:42:57 +0100

    Zmieniono zad1

--- src/Zad1.java
+++ src/Zad1.java
@@ -5,19 +5,11 @@ import java.util.Set;
 
 public class Zad1 {
     public static double Average_of_elements(int[][] arr, int p ){
-        String p1 =String.valueOf(p);
-        char[]p1_cyfry=p1.toCharArray();
-        Integer[]p1_cyfry_int=new Integer[p1_cyfry.length];
-        for(int z=0;z<p1_cyfry.length;z++){
-            p1_cyfry_int[z]=Character.getNumericValue(p1_cyfry[z]);
-        }
-        List<Integer> list= Arrays.asList(p1_cyfry_int);
-        Set<Integer> p2=new HashSet<>(list);
         double sum=0;
         int counter=0;
         for (int i=0;i<arr.length;i++){
             for(int j=0;j<arr[i].length;j++){
-                if(j%2==0 && p2.contains(i) ){
+                if(j%2==0 && cyfry(p).contains(i) ){
                     sum=sum+arr[i][j];
                     counter++;
                 }
@@ -26,5 +18,16 @@ public class Zad1 {
 
         return sum/counter;
     }
+private static Set cyfry(int x){
+    String p1 =String.valueOf(x);
+    char[]p1_cyfry=p1.toCharArray();
+    Integer[]p1_cyfry_int=new Integer[p1_cyfry.length];
+    for(int z=0;z<p1_cyfry.length;z++){
+        p1_cyfry_int[z]=Character.getNumericValue(p1_cyfry[z]);
+    }
+    List<Integer> list= Arrays.asList(p1_cyfry_int);
+    Set<Integer> p2=new HashSet<>(list);
+    return p2;
+}
 
 }
